0605cfe416bbcfe92037e7c11c9736a44a47ac63
K4YT3X <k4t@protonmail.com>
2017-09-16 01:21:11 -0400

    0.0.1: added basic stuff

--- defenseMatrix.py
+++ defenseMatrix.py
@@ -4,10 +4,61 @@ Name: K4YT3X
 Date Created: SEP 16, 2017
 Last Modified: SEP 16, 2017
 
+Licensed under the GNU General Public License Version 3 (GNU GPL v3),
+    available at: https://www.gnu.org/licenses/gpl-3.0.txt
+
+(C) 2016 - 2017 K4YT3X
+(C) 2017 fa11en
+
 """
+import psutil
+import os
+import urllib.request
 
-VERSION = "0.1"
 
+try:
+    import avalon_framework as avalon
+except ImportError:
+    while True:
+        install = input('\033[31m\033[1mAVALON Framework not installed! Install now? [Y/n] \033[0m')
+        if len(install) == 0 or install[0].upper() == 'Y':
+            try:
+                if os.path.isfile('/usr/bin/pip3'):
+                    print('Installing using method 1')
+                    os.system('pip3 install avalon_framework')
+                elif os.path.isfile('/usr/bin/wget'):
+                    print('Installing using method 2')
+                    os.system('wget -O - https://bootstrap.pypa.io/get-pip.py | python3')
+                    os.system('pip3 install avalon_framework')
+                else:
+                    print('Installing using method 3')
+                    # import urllib.request
+                    content = urllib.request.urlopen('https://bootstrap.pypa.io/get-pip.py')
+                    with open('/tmp/get-pip.py', 'w') as getpip:
+                        getpip.write(content.read().decode())
+                        getpip.close()
+                    os.system('python3 /tmp/get-pip.py')
+                    os.system('pip3 install avalon_framework')
+                    os.remove('/tmp/get-pip.py')
+            except Exception as e:
+                print('\033[31mInstallation failed!: ' + str(e))
+                print('Please check your Internet connectivity')
+                exit(0)
+            print('\033[32mInstallation Succeed!\033[0m')
+            print('\033[32mPlease restart the program\033[0m')
+            exit(0)
+        elif install[0].upper() == 'N':
+            print('\033[31m\033[1mSCUTUMM requires avalon framework to run!\033[0m')
+            print('\033[33mAborting..\033[0m')
+            exit(0)
+        else:
+            print('\033[31m\033[1mInvalid Input!\033[0m')
+
+
+VERSION = "0.01"
+
+
+# -------------------------------- Classes --------------------------------
 
 class iptables:
 
@@ -25,3 +76,10 @@ class rootkit:
 
     def __init__(self):
         pass
+
+
+# -------------------------------- Functions --------------------------------
+
+
+
+# -------------------------------- Procedural --------------------------------
