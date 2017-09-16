40b1a02edde53dcac21b60762b024c67c80a2574
conrad <conrad.heidebrecht@gmail.com>
2017-09-16 05:27:48 -0400

    0.0.2 Install

--- install.py
+++ install.py
@@ -28,9 +28,9 @@ class Install:
         self._install_config()
 
     def uninstall(self):
-        for method in self._installed:
-            if self._installed[method]:
-                exec('self._install_%s.uninstall()' % method)
+        for method in self._installed.keys():
+            # if self._installed[method]:
+            exec('self._install_%s(remove=True)' % method)
 
         sys.exit(0)
         exit(0)
@@ -38,36 +38,52 @@ class Install:
     def check_install(self):
         return os.path.exists(st.CONFPATH)
 
-    def _install_iptables(self):
-        pass
+    def _install_iptables(self, remove=False):
+        def uninstall():
+            pass
+        if remove:
+            return uninstall()
 
-    def _install_arptables(self):
-        def uninstall(self):
+    def _install_arptables(self, remove=False):
+        def uninstall():
             os.system(st.gen_pack_remove(st.package_manager, 'arptables'))
+        if remove:
+            return uninstall()
 
         # check arptables installation
         if not (os.path.isfile('/usr/bin/arptables')
                 or os.path.isfile('/usr/sbin/arptables')):
-            if not os.system(st.gen_pack_install(st.package_manager, 'arptables')):
+            if os.system(st.gen_pack_install(st.package_manager, 'arptables')):
                 print('Invalid package manager. Unable to proceed. ')
         else:
             av.error('arptables not installed. Unable to proceed. ' +
-                'Aborting...')
-            self._install_arptables.uninstall()
+                     'Aborting...')
+            self._install_arptables().uninstall()
 
         # retrieve method name and set its respective value in _installed
         self._installed[inspect.stack()[0][3].rsplit('_', 1)[-1]] = True
 
+    def _install_rkhunt(self, remove=False):
+        def uninstall():
+            pass
+        if remove:
+            return uninstall()
 
-    def install_rkhunt(self):
-        pass
+    def _install_tripwire(self, remove=False):
+        def uninstall():
+            pass
+        if remove:
+            return uninstall()
 
-    def _install_tripwire(self):
-        pass
+    def _install_passwdcmplx(self, remove=False):
+        def uninstall():
+            pass
+        if remove:
+            return uninstall()
 
-    def _install_passwdcmplx(self):
-        pass
-
-    def _install_config(self):
-        pass
+    def _install_config(self, remove=False):
+        def uninstall():
+            pass
+        if remove:
+            return uninstall()
 
