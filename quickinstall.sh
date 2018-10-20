0b486d514398c9eeb1326df19dbd7d59820c79c4
K4YT3X <k4yt3x@k4yt3x.com>
2018-10-20 17:42:51 -0400

    1.0.1 check user UUID, updated project git link

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-# Name: SCUTUM QuickInstall Script
+# Name: Defense Matrix Quick Installation Script
 # Author: K4YT3X
 # Date Created: Sep 27, 2017
-# Last Modified: Sep 29, 2017
+# Last Modified: October 19, 2018
+# Version 1.0.1
 
-# Description: Installs SCUTUM
-
-# Version 1.0
-
-if [ -d "/usr/share/DefenseMatrix/" ]; then
-  echo "Removing old DefenseMatrix files..."
-  rm -rf /usr/share/DefenseMatrix/
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
 fi
 
+# Remove older versions of defense matrix
+if [ -d "/usr/share/defense-matrix/" ]; then
+  echo "Removing old defense matrix files..."
+  rm -rf /usr/share/defense-matrix/
+fi
 
-git clone https://github.com/K4YT3X/DefenseMatrix.git /usr/share/DefenseMatrix
-cd /usr/share/DefenseMatrix
-/usr/bin/env python3 defenseMatrix.py --install
+# Clone the newest version of defense matrix
+git clone https://github.com/K4YT3X/defense-matrix.git /usr/share/defense-matrix
+
+# Run the installation script
+/usr/bin/env python3 /usr/share/defense-matrix/bin/defense-matrix.py --install
